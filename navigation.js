93ad9f794ebc2e45f9876e2aeb94ab103a9844ae
Pedro Guerrero <pjgm_18@hotmail.com>
2022-06-10 16:11:38 -0500

    vamos bien

---
+++ navigation.js
@@ -0,0 +1,73 @@
+contactLink.addEventListener('click', ()=> {
+    location.hash = '#contact' 
+})
+projectsLink.addEventListener('click', ()=> {
+    location.hash = '#projects' 
+})
+aboutLink.addEventListener('click', ()=> {
+    location.hash = '#about' 
+})
+logo.addEventListener('click', ()=> {
+    location.hash = '#home' 
+})
+
+
+
+
+
+
+
+//la funcion tambien cargara cuando cargue el DOM
+window.addEventListener('DOMContentLoaded', navigator, false)
+//La funcion que queremos ejecutar cada vez que cambie el hash
+window.addEventListener('hashchange', navigator, false);
+
+function navigator(){
+    console.log({location})
+
+    if(location.hash.startsWith('#contact')){
+        contactPage()
+    } else if(location.hash.startsWith('#projects')){
+        projectsPage()
+
+    }else if(location.hash.startsWith('#about')){
+        aboutPage()
+    }else if(location.hash.startsWith('#home')){
+        homePage()
+
+    } else{
+        homePage()
+    }
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
+
+function homePage(){
+    console.log('Estamos en el home')
+
+    projectSection.classList.add('inactive')
+    mainSection.classList.remove('inactive')
+    genericList.classList.add('inactive')
+
+    
+    /* getCategoriesPreview()
+    getTrendingMoviesPreview() */
+
+}
+
+function projectsPage(){
+    console.log('Estamos en pagina de proyectos')
+
+    genericList.classList.add('inactive')
+    mainSection.classList.add('inactive')
+    projectSection.classList.remove('inactive')
+
+}
+
+function contactPage(){
+    mainSection.classList.add('inactive')
+    projectSection.classList.add('inactive')
+    genericList.classList.add('inactive')
+    contacSection.classList.remove('inactive')
+}
+irvingvjuarez.netlify.app
---
+++ nodes.js
@@ -0,0 +1,14 @@
+
+//Section
+const header = document.querySelector('#header')
+const mainSection = document.querySelector('#mainSection')
+const projectSection = document.querySelector('#projectSection')
+const genericList = document.querySelector('#genericList')
+const contacSection = document.querySelector('#contactSection')
+
+
+//Elements
+const logo = document.querySelector('.logo')
+const contactLink = document.querySelector('.contactLink')
+const projectsLink = document.querySelector('.projectsLink')
+const aboutLink = document.querySelector('.aboutLink')
